c82a828b933541eff3a76b515839ff597216a5d4
juhyojeong <juhyojeong58@gmail.com>
2021-12-01 17:45:04 +0900

    Add: Navigation file
    Add Navigation index.js and Revise App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import Navigation from "./navigations";
+import { theme } from "./theme";
 
 const App = () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
+      <Navigation />
     </ThemeProvider>
   );
 };
