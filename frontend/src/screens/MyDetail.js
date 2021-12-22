6b18b713e857418bb3843602b4fc804b4ddae56d
juhyojeong <juhyojeong58@gmail.com>
2021-12-22 14:44:47 +0900

    Add: double check id
    Add double check in user update form

--- frontend/src/screens/MyDetail.js
+++ frontend/src/screens/MyDetail.js
@@ -22,6 +22,15 @@ const ErrorText = styled.Text`
   line-height: 20px;
   color: ${({ theme }) => theme.errorText};
 `;
+const ButtonContainer = styled.View`
+  width: 100%;
+  align-items: flex-end;
+  margin-top: -10px;
+  margin-bottom: -10px;
+`;
+const ButtonBox = styled.View`
+  width: 100px;
+`;
 
 const UserDetail = () => {
   const { user, dispatch } = useContext(UserContext);
@@ -32,6 +41,7 @@ const UserDetail = () => {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [isChecked, setIsChecked] = useState(true);
 
   const nameRef = useRef();
   const passwordRef = useRef();
@@ -45,6 +55,8 @@ const UserDetail = () => {
         _errorMsg = "Please enter yout name";
       } else if (!userId) {
         _errorMsg = "Please enter your id";
+      } else if (!isChecked) {
+        _errorMsg = "Please double check id";
       } else if (password.length < 6) {
         _errorMsg = "Password must contain 6 character at least";
       } else if (password !== passwordConfirm) {
@@ -59,7 +71,14 @@ const UserDetail = () => {
   }, [username, userId, password, passwordConfirm]);
   useEffect(() => {
     setDisabled(
-      !(username && userId && password && passwordConfirm && !errorMsg),
+      !(
+        username &&
+        userId &&
+        isChecked &&
+        password &&
+        passwordConfirm &&
+        !errorMsg
+      ),
     );
   }, [username, userId, password, passwordConfirm, errorMsg]);
 
@@ -83,6 +102,27 @@ const UserDetail = () => {
         Alert.alert(err.message);
       });
   };
+  const _handleDoubleCheck = () => {
+    if (user.userId === userId) {
+      Alert.alert("사용 가능한 아이디입니다");
+      setIsChecked(true);
+    } else {
+      axios
+        .get("http://10.0.2.2:8000/api/user/doublecheck/" + userId)
+        .then((res) => {
+          if (res.data.data === 0) {
+            Alert.alert("사용 가능한 아이디입니다");
+            setIsChecked(true);
+          } else {
+            Alert.alert("이미 사용중인 아이디입니다");
+            setIsChecked(false);
+          }
+        })
+        .catch((err) => {
+          Alert.alert(err.message);
+        });
+    }
+  };
 
   return (
     <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}>
@@ -97,6 +137,16 @@ const UserDetail = () => {
           placeholder="Id"
           returnKeyType="next"
         />
+        <ButtonContainer>
+          <ButtonBox>
+            <Button
+              title="중복확인"
+              onPress={_handleDoubleCheck}
+              disabled={user.userId === ""}
+              isFilled={false}
+            />
+          </ButtonBox>
+        </ButtonContainer>
         <Input
           ref={nameRef}
           label="Name"
